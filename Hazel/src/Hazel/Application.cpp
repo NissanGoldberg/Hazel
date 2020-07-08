b2ef44cc058f1af2e968d0de387e04284f66e3f2
Nissan Goldberg <nissan.goldberg@gamil.com>
2020-07-08 09:34:13 +0300

    10 - GLFW, but with Window::Create error

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -9,20 +9,17 @@
 namespace Hazel {
 
 	Application::Application() {
-	
+		//m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 	Application::~Application() {
 	}
 
-	void Application::Run(){
-		WindowResizeEvent e(1200, 720);
-		
-		if(e.IsInCategory(EventCategoryApplication))
-			HZ_TRACE(e); //<-- logs this
-		if (e.IsInCategory(EventCategoryInput))
-			HZ_TRACE(e);
+	void Application::Run() {
 
-		while (true);
+		//while (m_Running) {
+		//	m_Window->OnUpdate();
+		//}
+
 	}
 }
